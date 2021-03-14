bf4dd8fa3e574c66aef924c1e3217f41d1865551
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-03-14 02:52:52 -0300

    MRC Products

---
+++ src/routes/product.js
@@ -0,0 +1,11 @@
+const express = require(`express`)
+const router = express.Router()
+const productController = require(`../controller/product`)
+const {adminAuth, authToken} = require(`../middleware/auth`)
+/* Product Routes */
+    router.post   ( `/`,     adminAuth,  productController.create  )
+    router.get    ( `/`,     authToken,  productController.list    )
+    router.get    ( `/:id`,  authToken,  productController.find    )
+    router.put    ( `/:id`,  adminAuth,  productController.update  )
+    router.delete ( `/:id`,  adminAuth,  productController.delete  )
+module.exports = router
