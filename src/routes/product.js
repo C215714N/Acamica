c6069602974a136ab6230a2bb8ffa66502b86a4b
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-03-31 02:13:38 -0300

    JWT Validation Access

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,11 +1,11 @@
 const express = require(`express`)
 const router = express.Router()
 const productController = require(`../controller/product`)
-const {adminAuth, authToken} = require(`../middleware/auth`)
+const {adminAuth, authAccess} = require(`../middleware/auth`)
 /* Product Routes */
     router.post   ( `/`,     adminAuth,  productController.create  )
-    router.get    ( `/`,     authToken,  productController.list    )
-    router.get    ( `/:id`,  authToken,  productController.find    )
+    router.get    ( `/`,     authAccess, productController.list    )
+    router.get    ( `/:id`,  authAccess, productController.find    )
     router.put    ( `/:id`,  adminAuth,  productController.update  )
     router.delete ( `/:id`,  adminAuth,  productController.delete  )
 module.exports = router
